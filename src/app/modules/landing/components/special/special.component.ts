ae977a79feab3589be08ac1489c1332f71967fe1
ahmad shadcome <a.shadcome@gmail.com>
2021-01-21 17:36:52 +0330

    13991102

--- src/app/modules/landing/components/special/special.component.ts
+++ src/app/modules/landing/components/special/special.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AdverService } from 'src/app/services/adver.service';
+import * as $ from 'jquery';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'sc-special',
   templateUrl: './special.component.html',
   styleUrls: ['./special.component.scss']
 })
-export class SpecialComponent implements OnInit {
+export class SpecialComponent implements OnInit, AfterViewInit {
 
   advs: any[] = [];
   responsiveOptions;
+  menus: MenuItem[] = [];
 
   constructor(private sAdver: AdverService) {
     this.responsiveOptions = [
@@ -35,6 +38,27 @@ export class SpecialComponent implements OnInit {
     this.sAdver.getData().subscribe((res: any) => {
       this.advs = res.data;
     });
+
+    this.menus = [
+      { label: 'Istanbul', id: 'Istanbul' },
+      { label: 'Ankara', id: 'Ankara' },
+      { label: 'Izmir', id: 'Izmir' },
+      { label: 'Antalya', id: 'Antalya' },
+      { label: 'Konya', id: 'Konya' }
+    ];
+  }
+
+  ngAfterViewInit(): void {
+    $('#menu0').addClass('active');
+    $('#dash0').addClass('dash');
+  }
+
+  menuClicked(i): void {
+    $('[id^=menu]').removeClass('active');
+    $('#menu' + i).addClass('active');
+
+    $('[id^=dash]').removeClass('dash');
+    $('#dash' + i).addClass('dash');
   }
 
 }
