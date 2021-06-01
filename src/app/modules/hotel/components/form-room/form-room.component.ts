77afb0b9615dea0436edce412d7fa350c8608bf7
shadcome <a.shadcome@gmail.com>
2021-06-01 19:54:49 +0430

    تغییرات لیست اتاق

--- src/app/modules/hotel/components/form-room/form-room.component.ts
+++ src/app/modules/hotel/components/form-room/form-room.component.ts
@@ -19,33 +19,31 @@ export class FormRoomComponent implements OnInit {
   facilities: any[] = [];
   facilitiesKinds: any[] = [];
   saving: boolean;
- 
-    constructor(
-      private srvData: DataService,
-      private srvSrch: SearchService,
-      private srvHotel: HotelService,
-      private srvMsg: MessageService,
-      private router: Router,
-      private aRoute: ActivatedRoute,
-      private confirmationService: ConfirmationService,
-    ) { }
+
+  constructor(
+    private srvData: DataService,
+    private srvSrch: SearchService,
+    private srvHotel: HotelService,
+    private srvMsg: MessageService,
+    private router: Router,
+    private aRoute: ActivatedRoute,
+    private confirmationService: ConfirmationService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   submit(): void {
     this.saving = true;
-   
-      this.srvHotel.addRoom(this.rooms).subscribe(res => {
-        this.saving = false;
-        this.srvMsg.add({ severity: 'success', summary: 'ثبت  اطلاعات', detail: 'ثبت اطلاعات با موفقیت انجام شد .' });
-        this.router.navigate(['./panel/hotel/rooms']);
-      }, _ => {
-        this.saving = false;
-      });
-    
-
+    this.srvHotel.addRoom(this.rooms).subscribe(res => {
+      this.saving = false;
+      this.srvMsg.add({ severity: 'success', summary: 'ثبت  اطلاعات', detail: 'ثبت اطلاعات با موفقیت انجام شد .' });
+      this.router.navigate(['./panel/hotel/rooms']);
+    }, _ => {
+      this.saving = false;
+    });
   }
+
   getHotels(event: any): void {
     this.srvData.showMainProgressBarForMe();
     this.srvSrch.getHotel(event.query).subscribe(res => {
@@ -53,6 +51,7 @@ export class FormRoomComponent implements OnInit {
       this.srvData.thanksMainProgressBar();
     });
   }
+
   getRoomkinds(event: any): void {
     this.srvData.showMainProgressBarForMe();
     this.srvSrch.getHotelRoom(event.query).subscribe(res => {
@@ -60,11 +59,13 @@ export class FormRoomComponent implements OnInit {
       this.srvData.thanksMainProgressBar();
     });
   }
+
   getFacilities(event: any): void {
     this.srvSrch.getHotelRoomFacilitiesKind(event.query).subscribe(res => {
       this.facilities = res;
     });
   }
+
   addRoom(): void {
     this.rooms.push({});
   }
