2784b1f19714a9d0af74028b7f4b42bfe5ce9aff
shokri <amirmohammadshokri2019@gmail.com>
2021-06-20 19:50:43 +0430

    add form user anf create company

--- src/app/modules/company/company.module.ts
+++ src/app/modules/company/company.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CompanyRoutingModule } from './company-routing.module';
 import { ListUserComponent } from './components/list-user/list-user.component';
 import { ListCompanyComponent } from './components/list-company/list-company.component';
+import { FormCompanyComponent } from './components/form-company/form-company.component';
+import { FormUserComponent } from './components/form-user/form-user.component';
 
 
 @NgModule({
-  declarations: [ListUserComponent, ListCompanyComponent],
+  declarations: [ListUserComponent, ListCompanyComponent, FormCompanyComponent, FormUserComponent],
   imports: [
     CommonModule,
     CompanyRoutingModule
