2ad3e24ceaada5469c7a3cd3b49051e989daf9a5
shokri <amirmohammadshokri2019@gmail.com>
2021-08-18 02:08:34 +0430

    add report

---
+++ src/app/modules/promotion/components/report-promotion/report-promotion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ss-report-promotion',
+  templateUrl: './report-promotion.component.html',
+  styleUrls: ['./report-promotion.component.scss']
+})
+export class ReportPromotionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
