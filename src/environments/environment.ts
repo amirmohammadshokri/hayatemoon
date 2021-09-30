378f1526df4cf8c9518bda9a0444f0c003daff7c
ahmad shadcome <a.shadcome@gmail.com>
2021-09-30 19:41:09 +0330

    ثدر

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,25 @@ export const environment = {
     stsServer: 'https://identity.gashtineh.com',
     redirectUrl: 'https://localhost:4200',
     postLogoutRedirectUri: 'https://localhost:4200',
+    clientId: 'Gashtineh-adminPanel-client',
+    scope: 'openid profile offline_access Gashtineh-api roles',
+    secret: '3FA5DBA3-3EB0-4049-8E58-FDA932F5F0B0',
+    responseType: 'code',
+    silentRenew: true,
+    silentRenewUrl: 'https://localhost:4200/silent-renew.html',
+    authority: 'https://identity.gashtineh.com',
+    useRefreshToken: true,
+    logLevel: LogLevel.Debug,
+  },
+};
+
+/*
+export const environment = {
+  production: false,
+  SSoConfiguration: {
+    stsServer: 'https://identity.gashtineh.com',
+    redirectUrl: 'https://localhost:4200',
+    postLogoutRedirectUri: 'https://localhost:4200',
     clientId: 'Gashtineh-adminPanel-client-test',
     scope: 'openid profile offline_access Gashtineh-api roles',
     secret: 'FDE4A2A7-8204-4946-9949-2625BE23EBDA',
@@ -24,12 +43,4 @@ export const environment = {
     history_cleanup_off: true
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
